450762c1bb71acfee3358f153f8757dd0615a510
TehilaFried <49166882+TehilaFried@users.noreply.github.com>
2021-08-24 20:18:45 +0300

    confirm password and pre save middleware

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcrypt');
 
 const UserSchema = mongoose.Schema({
     fullName: {
@@ -12,10 +13,31 @@ const UserSchema = mongoose.Schema({
         maxLength: 16,
         required:[true, "can't be blank"]
     },
+    passwordConfirm: {
+        type: String,
+        required:[true, "The passwords must be the same"],
+        validate:{
+            validator:function(e){
+                return this.password===e;
+            }
+        }
+    },
     email:{
         type:String,
         validate:validator.isEmail()
     }
     
 });
+
+UserSchema.pre('save', async function (next) {
+    
+    if(this.isModified('password')){
+    const rounds = 12; // What you want number for round paasword
+
+    const hash = await bcrypt.hash(this.password, rounds);
+    this.password = hash;
+    this.passwordConfirm=undefined;
+    }
+    next()
+  })
 module.exports = mongoose.model('User', UserSchema);
